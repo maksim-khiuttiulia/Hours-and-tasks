9e1240228959f37563a3105ab4e64910dd426087
maksim-khiuttiulia <43935623+maksim-khiuttiulia@users.noreply.github.com>
2020-04-07 21:06:30 +0200

    init commit

---
+++ src/main/java/com/mk/hoursandtasks/HoursAndTasksApplication.java
@@ -0,0 +1,13 @@
+package com.mk.hoursandtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoursAndTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoursAndTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mk/hoursandtasks/HoursAndTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.hoursandtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoursAndTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
