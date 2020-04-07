f713b3231daf014c425d367d81beb2c3831a4d99
maksim-khiuttiulia <43935623+maksim-khiuttiulia@users.noreply.github.com>
2020-04-07 23:10:30 +0200

    added grid

--- src/main/webapp/src/components/task.jsx
+++ src/main/webapp/src/components/task.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import TaskLabel from './task-label'
-import { ListGroupItem, ListGroupItemHeading, ButtonGroup, Button } from 'reactstrap';
+import { ListGroupItem, ButtonGroup, Button, Row, Col} from 'reactstrap';
 
 export default class Task extends Component {
     constructor(props) {
@@ -8,31 +8,31 @@ export default class Task extends Component {
         this.state = {
             id: this.props.task.id,
             name: this.props.task.name,
-            priority : this.props.task.priority,
+            priority: this.props.task.priority,
             labels: this.props.task.labels,
             isDone: this.props.task.isDone
         }
     }
 
-    onDelete = (e) =>{
+    onDelete = (e) => {
         this.props.onDelete(e.target.value)
     }
 
-    onDone = (e) =>{
+    onDone = (e) => {
         this.props.onChangeStatus(e.target.value)
     }
 
-    onNotDone = (e) =>{
+    onNotDone = (e) => {
         this.props.onChangeStatus(e.target.value)
     }
 
     render() {
         const labelsElement = this.state.labels.map((label) => {
-            return <TaskLabel label={label} key={label.id}/>;
+            return <TaskLabel label={label} key={label.id} />;
         })
 
         let buttonState;
-        if (this.state.isDone){
+        if (this.state.isDone) {
             buttonState = <Button color="info" onClick={this.onNotDone} value={this.state.id}>Not done</Button>
         } else {
             buttonState = <Button color="success" onClick={this.onDone} value={this.state.id}>Done</Button>
@@ -41,17 +41,26 @@ export default class Task extends Component {
 
         return (
             <ListGroupItem color={this.state.isDone ? "success" : "danger"} className="d-flex justify-content-between align-items-center" key={this.state.id}>
-                <ListGroupItemHeading>{this.state.name}</ListGroupItemHeading>
-                <div className="pull-right d-flex align-items-center">
-                    <div className="mr-2">
-                        {labelsElement}
-                    </div>
-                    
-                    <ButtonGroup>
-                        {buttonState}
-                        {buttonDelete}
-                    </ButtonGroup>
-                </div>
+
+                <Row className="d-flex align-items-center">
+                    <Col xs="12" md="6">
+                        <Row>
+                            <Col xs="12">
+                                <h4>{this.state.name}</h4>
+                            </Col>
+                            <Col xs="12">
+                                {labelsElement}
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs="12" md="6" className="d-flex">
+                        <ButtonGroup>
+                            {buttonState}
+                            {buttonDelete}
+                        </ButtonGroup>
+                    </Col>
+                </Row>
+
 
             </ListGroupItem>
         );
