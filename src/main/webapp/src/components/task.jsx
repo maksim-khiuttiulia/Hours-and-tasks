402a837fb1729fd484fd8d25e2a4261da0023de7
maksim-khiuttiulia <43935623+maksim-khiuttiulia@users.noreply.github.com>
2020-04-12 14:15:48 +0200

    changed state in task.jsx

--- src/main/webapp/src/components/task.jsx
+++ src/main/webapp/src/components/task.jsx
@@ -1,27 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import TaskLabel from './task-label'
 import TaskDeadLineLabel from './task-deadline'
-import { ListGroupItem, ButtonGroup, Button, Row, Col } from 'reactstrap';
+import {Button, ButtonGroup, Col, ListGroupItem, Row} from 'reactstrap';
 
 export default class Task extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: this.props.task.id,
-            name: this.props.task.name,
-            done: this.props.task.done,
-            text: this.props.task.text,
-            created: this.props.task.created,
-            deadline: this.props.task.deadline,
-            projectId: this.props.task.projectId,
-            labels: this.props.task.labels,
+            task : this.props.task
         }
     }
 
     onDelete = (e) => {
         e.preventDefault();
         let task = {
-            ...this.state,
+            ...this.state.task,
         }
         this.props.onDeleteTask(task)
     }
@@ -29,7 +22,7 @@ export default class Task extends Component {
     onDone = (e) => {
         e.preventDefault();
         let task = {
-            ...this.state,
+            ...this.state.task,
             done : true
         }
         this.props.onChangeTaskStatus(task)
@@ -38,7 +31,7 @@ export default class Task extends Component {
     onNotDone = (e) => {
         e.preventDefault();
         let task = {
-            ...this.state,
+            ...this.state.task,
             done : false
         }
         this.props.onChangeTaskStatus(task)
@@ -46,12 +39,12 @@ export default class Task extends Component {
 
     render() {
         const deadlineLabel = <TaskDeadLineLabel deadline={this.state.deadline}/>
-        const labelsElement = this.state.labels.map((label) => {
+        const labelsElement = this.state.task.labels.map((label) => {
             return <TaskLabel label={label} key={label.id} />;
         })
 
         let buttonState;
-        if (this.state.done) {
+        if (this.state.task.done) {
             buttonState = <Button color="info" onClick={this.onNotDone} style={{ minWidth: 100 }}>Not done</Button>
         } else {
             buttonState = <Button color="success" onClick={this.onDone} style={{ minWidth: 100 }}>Done</Button>
@@ -59,13 +52,13 @@ export default class Task extends Component {
         let buttonDelete = <Button color="danger" onClick={this.onDelete}  style={{ minWidth: 100 }}>Delete</Button>
 
         return (
-            <ListGroupItem color={this.state.done ? "success" : "danger"} className="justify-content-between" key={this.state.id}>
+            <ListGroupItem color={this.state.task.done ? "success" : "danger"} className="justify-content-between" key={this.state.task.id}>
 
                 <Row className="d-flex align-items-center">
                     <Col xs="6" md="6">
                         <Row>
                             <Col xs="12">
-                                <h4>{this.state.name}</h4>
+                                <h4>{this.state.task.name}</h4>
                             </Col>
                             <Col xs="12">
                                 {deadlineLabel}
